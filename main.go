b41c04e4f6b0ee612fe78c107cd0a113e14d2244
Creigh Battle <creighbattle@gmail.com>
2024-07-09 21:00:07 -0400

    serving basic html file

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/", fileServer)
 
 	server := &http.Server{
 		Addr: "localhost:8080",
