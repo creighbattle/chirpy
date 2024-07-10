3c907850586d7bca298678a2ba20e36192c01357
Creigh Battle <creighbattle@gmail.com>
2024-07-10 15:22:55 -0400

    added a validate chirp handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -35,11 +36,48 @@ func (cfg * apiConfig) resetRequestCountHandler(w http.ResponseWriter, r *http.R
 	w.Write([]byte("Reset"))
 }
 
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	type validResponse struct {
+		Valid bool `json:"valid"`
+	}
+	w.Header().Set("Content-Type", "application/json")
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		w.WriteHeader(400)
+		dat, _ := json.Marshal(errorResponse{Error: "something went wrong"})
+		w.Write(dat)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		w.WriteHeader(400)
+		dat, _ := json.Marshal(errorResponse{Error: "Chirp is too long"})
+		w.Write(dat)
+		return
+	}
+
+	w.WriteHeader(200)
+	dat, _ := json.Marshal(validResponse{Valid: true})
+	w.Write(dat)
+}
+
 
 func main() {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("."))
 	fileServer = http.StripPrefix("/app", fileServer)
+
+	
 
 	config := apiConfig{
 		 fileserverHits: 0,
@@ -57,6 +95,9 @@ func main() {
 	
 	// Handle health check
 	mux.HandleFunc("GET /api/healthz", healthCheckHandler)
+
+	// Validate Chirp
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 
 	server := &http.Server{
