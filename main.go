5f44c2288b8ea1ff1ce697aff71d94bf89f3d752
Creigh Battle <creighbattle@gmail.com>
2024-07-09 21:41:54 -0400

    create custom handler to check status of server

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import "net/http"
 
+func myHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	responseWriter.WriteHeader(200)
+	responseWriter.Write([]byte("OK"))
+}
+
 
 func main() {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("."))
+	fileServer = http.StripPrefix("/app", fileServer)
 
-	mux.Handle("/", fileServer)
-	mux.Handle("/assets/logo.png", fileServer)
+	mux.Handle("/app/*", fileServer)
+	mux.Handle("/app/assets/logo.png", fileServer)
+
+	
+
+	mux.HandleFunc("/healthz", myHandler)
 
 	server := &http.Server{
 		Addr: "localhost:8080",
