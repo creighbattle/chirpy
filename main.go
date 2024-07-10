b093cf6436b4abe5f8f0f793b7b7fc80f33256c3
Creigh Battle <creighbattle@gmail.com>
2024-07-10 12:44:01 -0400

    created middleware to keep track of fileserver hits | created handlers to view hits and reset hits

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		
+		cfg.fileserverHits += 1
+		next.ServeHTTP(w, r)
+	})
+}
 
 func myHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	responseWriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	responseWriter.WriteHeader(200)
 	responseWriter.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) numOfRequestsHandler(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
+}
+
+func (cfg * apiConfig) resetRequestCountHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Reset"))
 }
 
 
@@ -14,12 +42,23 @@ func main() {
 	fileServer := http.FileServer(http.Dir("."))
 	fileServer = http.StripPrefix("/app", fileServer)
 
-	mux.Handle("/app/*", fileServer)
-	mux.Handle("/app/assets/logo.png", fileServer)
+	config := apiConfig{
+		 fileserverHits: 0,
+	}
 
 	
+	// Handle the main app route with the middleware
+	mux.Handle("/app/", config.middlewareMetricsInc(fileServer))
+	
+	// Handle the metrics endpoint
+	mux.HandleFunc("/metrics", config.numOfRequestsHandler)
 
+	// Handle the reset endpoint
+	mux.HandleFunc("/reset", config.resetRequestCountHandler)
+	
+	// Handle health check
 	mux.HandleFunc("/healthz", myHandler)
+
 
 	server := &http.Server{
 		Addr: "localhost:8080",
