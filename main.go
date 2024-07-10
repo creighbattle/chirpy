981902a78e62f507e4621e9956729907f5830ba4
Creigh Battle <creighbattle@gmail.com>
2024-07-09 21:06:17 -0400

    serving an image from the file server

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("."))
 
 	mux.Handle("/", fileServer)
+	mux.Handle("/assets/logo.png", fileServer)
 
 	server := &http.Server{
 		Addr: "localhost:8080",
